628f44436458f1f135caf201e28ffb96ba809b22
Wonu Lee <wonwoolee24@gmail.com>
2024-01-11 22:57:26 +0900

    add font families

--- src/App.tsx
+++ src/App.tsx
@@ -18,16 +18,21 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1 className="text-3xl font-bold underline">Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+			<h1 className="font-chelsea text-3xl font-bold underline">
+				Vite + React
+			</h1>
+			<div className="card font-mitr">
+				<button
+					className="font-mochiy"
+					onClick={() => setCount((count) => count + 1)}
+				>
 					count is {count}
 				</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">
+			<p className="font-senior read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
 		</>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		fontFamily: {
+			chelsea: "'Chelsea Market', system-ui",
+			mitr: "'Mitr', sans-serif",
+			mochiy: "'Mochiy Pop P One', sans-serif",
+			mogra: "'Mogra', system-ui",
+			senior: "'PC Senior', sans-serif",
+		},
 		extend: {},
 	},
 	plugins: [],
