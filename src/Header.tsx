6a4bbe29c9b20e13ca458367231682b20215fbb3
Wonu Lee <wonwoolee24@gmail.com>
2024-01-12 03:11:19 +0900

    add header contacts

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,11 @@
+import {
+	CoinGecko,
+	Github,
+	Discord,
+	Telegram,
+	Twitter,
+} from './icon/Contact.tsx'
+
 export default function Header() {
 	return (
 		<header>
@@ -11,6 +19,35 @@ export default function Header() {
 				<a href="">WALLETS</a>
 				<a href="">EXCHANGES</a>
 				<a href="">ORDINALS</a>
+			</nav>
+			<nav className="flex h-[68px] w-full items-center justify-end space-x-[30px] bg-[#FFE46A] pr-[calc(6200%/1920)] text-[#FFE46A]">
+				<a href="https://github.com/Nintondo" target="_blank" rel="noreferrer">
+					<Github fill="#E60012" />
+				</a>
+				<a
+					href="https://twitter.com/_Bellscoin"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<Twitter fill="#E60012" />
+				</a>
+				<a
+					href="https://t.me/BellsCoinOfficial"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<Telegram fill="#E60012" />
+				</a>
+				<a href="https://t.co/4DCHZEdSSE" target="_blank" rel="noreferrer">
+					<Discord fill="#E60012" />
+				</a>
+				<a
+					href="https://www.coingecko.com/en/coins/bellscoin"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<CoinGecko theme="red" />
+				</a>
 			</nav>
 		</header>
 	)
