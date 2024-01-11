42a3fdfadd84eab02207ad376d2a9adf8ab6c3ac
Wonu Lee <wonwoolee24@gmail.com>
2024-01-11 19:30:53 +0900

    add tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
 		'plugin:react/jsx-runtime',
 		'plugin:react-hooks/recommended',
 	],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	ignorePatterns: ['dist', '.eslintrc.cjs', '*.config.js'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
