a1410074c83e3cf91a58976155be6a2357d4f361
Wonu Lee <wonwoolee24@gmail.com>
2024-01-13 02:10:53 +0900

    apply svg sprites

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,3 @@
-import {
-	CoinGecko,
-	Github,
-	Discord,
-	Telegram,
-	Twitter,
-} from './icon/Contact.tsx'
-
 export default function Header() {
 	return (
 		<header>
@@ -22,31 +14,41 @@ export default function Header() {
 			</nav>
 			<nav className="flex h-[68px] w-full items-center justify-end space-x-[30px] bg-[#FFE46A] pr-[calc(6200%/1920)] text-[#FFE46A]">
 				<a href="https://github.com/Nintondo" target="_blank" rel="noreferrer">
-					<Github fill="#E60012" />
+					<svg height={36} width={36}>
+						<use href="/sprite.svg#github" fill="#E60012" />
+					</svg>
 				</a>
 				<a
 					href="https://twitter.com/_Bellscoin"
 					target="_blank"
 					rel="noreferrer"
 				>
-					<Twitter fill="#E60012" />
+					<svg height={32} viewBox="0 0 36 36">
+						<use href="/sprite.svg#twitter" fill="#E60012" />
+					</svg>
 				</a>
 				<a
 					href="https://t.me/BellsCoinOfficial"
 					target="_blank"
 					rel="noreferrer"
 				>
-					<Telegram fill="#E60012" />
+					<svg height={36} width={36}>
+						<use href="/sprite.svg#telegram" fill="#E60012" />
+					</svg>
 				</a>
 				<a href="https://t.co/4DCHZEdSSE" target="_blank" rel="noreferrer">
-					<Discord fill="#E60012" />
+					<svg height={36} width={36}>
+						<use href="/sprite.svg#discord" fill="#E60012" />
+					</svg>
 				</a>
 				<a
 					href="https://www.coingecko.com/en/coins/bellscoin"
 					target="_blank"
 					rel="noreferrer"
 				>
-					<CoinGecko theme="red" />
+					<svg height={36} width={36}>
+						<use href="/sprite.svg#coinGecko" id="coinGecko-red" />
+					</svg>
 				</a>
 			</nav>
 		</header>
